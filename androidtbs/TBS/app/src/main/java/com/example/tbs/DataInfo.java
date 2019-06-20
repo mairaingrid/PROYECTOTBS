d0cdadfe34255c07f290f1ab6159b1c04aa31a56
juliakarinaalamira <49422325+juliakarinaalamira@users.noreply.github.com>
2019-06-20 16:31:46 -0400

    datainfo principal
    data

---
+++ androidtbs/TBS/app/src/main/java/com/example/tbs/DataInfo.java
@@ -0,0 +1,31 @@
+package com.example.tbs;
+
+public class DataInfo {
+    private Integer img;
+    private String title;
+    private String descripcion;
+
+    public Integer getImg() {
+        return img;
+    }
+
+    public void setImg(Integer img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
