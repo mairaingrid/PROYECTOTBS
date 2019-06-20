a2e70f236da2d8611a38cfb23f792b899a120b2a
juliakarinaalamira <49422325+juliakarinaalamira@users.noreply.github.com>
2019-06-20 17:00:23 -0400

    añadido lista a la principal

--- androidtbs/TBS/app/src/main/java/com/example/tbs/pagina_principal.java
+++ androidtbs/TBS/app/src/main/java/com/example/tbs/pagina_principal.java
@@ -20,14 +20,126 @@ import java.util.ArrayList;
 
 public class pagina_principal extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+        private ArrayList<DataInfo> listdata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principal);
+        loadComponets();
+    }
+
+    private void loadComponets() {
+        ListView list = findViewById(R.id.listview);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        listdata = new ArrayList<>();
+        Integer[] images = new Integer[]{R.drawable.brand, R.drawable.fresa, R.drawable.fruts, R.drawable.uvas, R.drawable.brand};
+        String[] titles = new String[]{"AUTO", "CELULAR", "BICICLETA", "VENTILADOR", "DESPERTADOR"};
+        String[] descrip = new String[]{"vendo auto costo 25.000$ cantida 5,disponible", "vendo celular samsung costo 150bs", "vendo bicicleta costo 300bs cantidad 4, disponible", "vendo ventilador, costo 200bs, cantidad 5, disponible", "vendo despertador, costo 100bs,cantidad 10 , disponible"};
+        //ArrayList<DataInfo> listdata =new ArrayList<>();
+        for (int i = 0; i < images.length; i++) {
+            DataInfo info = new DataInfo();
+            info.setImg(images[i]);
+            info.setTitle(titles[i]);
+            info.setDescripcion(descrip[i]);
+            listdata.add(info);
+
+        }
+        MyAdapter adapter = new MyAdapter(this, listdata);
+
+        list.setAdapter(adapter);
+        //list.setOnItemClickListener(this);
+        //grid.setAdapter(adapter);
+        //spinner.setAdapter(adapter);
+
+
+
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.pagina_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.publicaranuncio) {
+            // Handle the camera action
+            Intent publicar_anuncio = new Intent(this, subir_foto.class);
+            startActivity(publicar_anuncio);
+
+
+        } else if (id == R.id.nav_mismensajes) {
+       //     Intent mismensajes= new Intent(this,mimensajes.class);
+           // startActivity(mismensajes);
+
+        } else if (id == R.id.nav_misfavoritos) {
+
+        } else if (id == R.id.mi_perfil) {
+            Intent irperfil = new Intent (this, miperfil.class);
+            startActivity(irperfil);
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
 
 
 
