4df0a11e4e1edbe589d2573073c638c6af6680be
mairaingrid <ingrid24may@hotmail.com>
2019-07-16 22:32:45 -0400

    Utils

---
+++ androidtbs/TBS/app/src/main/java/com/example/tbs/Utils/Data.java
@@ -0,0 +1,23 @@
+package com.example.tbs.Utils;
+
+public class Data {
+
+    public static String USER_ID = "";
+    public static String TOKEN = "";
+    public static String IP = "http://10.0.2.2:8000";//"http://192.168.1.102:8000"
+    public static String HOST = IP + "/api";
+    public static String HOST_USER = HOST + "/usuarios";
+    public static String HOST_USER_LOGIN = HOST_USER + "/login";
+    public static String HOST_PRODUCT = HOST + "/productos/";
+    public static String HOST_PRODUCT_SELLER = HOST_PRODUCT + "vendedor/"; //+"comprador/";
+    public static String HOST_FAVORITO = HOST + "/usuariosfavoritos";
+    public static String HOST_CITAS = HOST + "/citas";
+    public static String HOST_MENSAJE = HOST + "/mensaje";
+
+
+
+    //public static String HOST_IMAGES = HOST + "/imagenes/";
+
+
+}
+
